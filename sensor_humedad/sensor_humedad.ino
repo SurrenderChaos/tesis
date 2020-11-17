29451fd21abeed5ebd90818b2ab4545ed3e1e2c0
surrenderchaos <sandyclaus2015@gmail.com>
2020-11-16 19:55:06 -0500

    sesor de temperatura funciona con firebase y servo automatico de temperatura tambien

--- sensor_humedad/sensor_humedad.ino
+++ sensor_humedad/sensor_humedad.ino
@@ -13,20 +13,20 @@
 
 int servo_pin = D7;  // for ESP8266 microcontroller
 Servo myservo;
-
-
-void setup()
-{
-    // Definimos el pin digital donde se conecta el sensor
-    #define DHTPIN 2
+ // Definimos el pin digital donde se conecta el sensor
+    #define DHTPIN D1
     // Dependiendo del tipo de sensor
     #define DHTTYPE DHT11
-    myservo.attach(servo_pin);
+    
  
     // Inicializamos el sensor DHT11
     DHT dht(DHTPIN, DHTTYPE);
 
+void setup()
+{
+   
 
+    myservo.attach(servo_pin);
     // Connect to Wi-Fi
     Serial.print("Wi-Fi...");
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -42,18 +42,19 @@ void setup()
 
     Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
     Firebase.set("servo01", 0);
+    Firebase.set("temperatura01", 0);
     Serial.begin(9600);
     dht.begin();
 }
 
  
 void loop() {
-
+  Serial.println(WiFi.localIP());
   delay(1000);
-  Firebase.getInt("servo01");//codigo con el que se obtiene el valor del servo en firebase para moverlo 
+  //Firebase.getInt("servo01");//codigo con el que se obtiene el valor del servo en firebase para moverlo 
     // Esperamos 5 segundos entre medidas
 
-  delay(3000);
+  //delay(3000);
  
   // Leemos la humedad relativa
   float h = dht.readHumidity();
@@ -72,16 +73,15 @@ void loop() {
   float hif = dht.computeHeatIndex(f, h);
   // Calcular el índice de calor en grados centígrados
   float hic = dht.computeHeatIndex(t, h, false);
-  Firebase.setFloat("temperatura01", t);
-  //codigo para abrir una ventana cuando la temperatura supera los 25 grados 
   int angle = 0;
-  if(t >= 25){
-    angle = 90;
+  if(t > 19){
+    angle = 45;
     myservo.write(angle);
   }else{
     angle = 0;
     myservo.write(angle);
   }
+  Firebase.setFloat("temperatura01", t);
+  //codigo para abrir una ventana cuando la temperatura supera los 25 grados 
+  
 }
-
-
