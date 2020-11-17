5199c386ffebf7d030e0918aa6689a24e5b22df2
surrenderchaos <sandyclaus2015@gmail.com>
2020-11-16 22:24:09 -0500

    codigo que funciona sensor de temperatura, servo automatico y servo manual

--- sensor_humedad/sensor_humedad.ino
+++ sensor_humedad/sensor_humedad.ino
@@ -41,47 +41,57 @@ void setup()
     Serial.println(WiFi.localIP());
 
     Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-    Firebase.set("servo01", 0);
+    //Firebase.set("servo01", 0);
     Firebase.set("temperatura01", 0);
-    Serial.begin(9600);
+    //Serial.begin(9600);
     dht.begin();
 }
 
  
 void loop() {
   Serial.println(WiFi.localIP());
-  delay(1000);
-  //Firebase.getInt("servo01");//codigo con el que se obtiene el valor del servo en firebase para moverlo 
+  delay(3000);
+  ;//codigo con el que se obtiene el valor del servo en firebase para moverlo 
     // Esperamos 5 segundos entre medidas
 
   //delay(3000);
  
   // Leemos la humedad relativa
-  float h = dht.readHumidity();
+  //float h = dht.readHumidity();
   // Leemos la temperatura en grados centígrados (por defecto)
   float t = dht.readTemperature();
   // Leemos la temperatura en grados Fahreheit
-  float f = dht.readTemperature(true);
+  //float f = dht.readTemperature(true);
  
   // Comprobamos si ha habido algún error en la lectura
-  if (isnan(h) || isnan(t) || isnan(f)) {
+  /*if (isnan(h) || isnan(t) || isnan(f)) {
     Serial.println("Error obteniendo los datos del sensor DHT11");
     return;
-  }
+  }*/
  
   // Calcular el índice de calor en Fahreheit
-  float hif = dht.computeHeatIndex(f, h);
+  //float hif = dht.computeHeatIndex(f, h);
   // Calcular el índice de calor en grados centígrados
-  float hic = dht.computeHeatIndex(t, h, false);
-  int angle = 0;
-  if(t > 19){
-    angle = 45;
-    myservo.write(angle);
-  }else{
-    angle = 0;
-    myservo.write(angle);
-  }
+  //float hic = dht.computeHeatIndex(t, h, false);
   Firebase.setFloat("temperatura01", t);
-  //codigo para abrir una ventana cuando la temperatura supera los 25 grados 
+  
+  int estado = Firebase.getInt("modo_servo");
+//este codigo permite cambiar de un modo automatico de movimiento del servo
+//que depende de la temperatura, a un modo manual en el que se define el angulo
+//del servo mediante la app
+  if(estado == 1){
+    if(t >= 20){
+      myservo.write(Firebase.getInt("angulo_auto_servo"));
+    }else{
+      myservo.write(0);
+    }
+    
+  }else {
+    myservo.write(Firebase.getInt("servo01"));
+  }
+  
+ 
+  
+  
   
 }
