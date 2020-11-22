550cb62ebc0c21e2e4ce144ce9f04ba1dfdee908
surrenderchaos <sandyclaus2015@gmail.com>
2020-11-22 00:30:31 -0500

    codigo para recuperar en tiempo real datos de firebase

--- Home_Automation/App.js
+++ Home_Automation/App.js
@@ -2,19 +2,32 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from './database/firebase'
 
 export default function App() {
+  // Define the state of the component
+  const [temperatura, setTemperatura] = useState(0);
+
+  // Listen to changes on the firebase database, specifically the "distance" entry
+  useEffect(() => {
+    const getValue = firebase.database().ref("temperatura01");
+    getValue.on("value", snapshot => {
+      let value = snapshot.val();
+      // Whenever the value changes on the server, it is also reset on the running app through the variable
+      setTemperatura(value.toFixed(2));
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>cambio de prueba windows</Text>
-      <Text>cambio de prueba</Text>
-      <Text>un tercer cambio en linux</Text>
-      <StatusBar style="auto" />
-    </View>
+    <div className="litreDisplay">
+      <div className="displayValue">
+        <span>{temperatura}c</span>
+      </div>
+    </div>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
