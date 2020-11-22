aa1d475c0fb980b0bea4e77bf4954daab5dc95ee
surrenderchaos <sandyclaus2015@gmail.com>
2020-11-21 22:03:11 -0500

    configuracion de firebase

---
+++ Home_Automation/database/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase"
+import "firebase/database"
+var firebaseConfig = {
+    apiKey: "AIzaSyCuwE2wHaJxt29pFiOYD6FVlTLNECz1sxc",
+    authDomain: "homeautomation-cfe46.firebaseapp.com",
+    databaseURL: "https://homeautomation-cfe46.firebaseio.com",
+    projectId: "homeautomation-cfe46",
+    storageBucket: "homeautomation-cfe46.appspot.com",
+    messagingSenderId: "776741220068",
+    appId: "1:776741220068:web:d010e48f97c9bbcd4bc6f1",
+    measurementId: "G-B4E446X91C"
+  };
+  firebase.initializeApp(firebaseConfig);
+  export default firebase;
+
+  
