661e2c1f6b23613aaae4e26715d53a822de675db
surrenderchaos <sandyclaus2015@gmail.com>
2020-11-24 14:50:05 -0500

    codigo para rele funcionando

--- Home_Automation/App.js
+++ Home_Automation/App.js
@@ -4,11 +4,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { AppRegistry, StyleSheet, View, Text, processColor  } from "react-native";
+import { AppRegistry, StyleSheet, View, Text, processColor, Button, Switch  } from "react-native";
 import Slider from '@react-native-community/slider';
 import firebase from './database/firebase';
-import { BarChart, Grid } from 'react-native-svg-charts'
-
+import { BarChart, Grid, XAxis, YAxis } from 'react-native-svg-charts'
 
 function Temper(){
   const [temperatura, setTemperatura] = useState(0);
@@ -25,20 +24,50 @@ function Temper(){
   }, []);
   return temperatura;        
 }
+function LuzSensor(){
+  const [luz, setLuz] = useState(0);
+  // Listen to changes on the firebase database, specifically the "distance" entry
+  useEffect(() => {
+    const getValue = firebase.database().ref("sensor_luz");
+    //al parecer existe diferencia en los datos del firebase por lo que se debe cambiar de datos si da un error con este codigo
+    getValue.on("value", snapshot => {
+      // Whenever the value changes on the server, it is also reset on the running app through the variable
+      let value = snapshot.val();
+      setLuz(value);
+    });
+
+  }, []);
+  return temperatura;        
+}
 class BarChartExample extends React.PureComponent {
 
   
     render() {
         const fill = 'rgb(134, 65, 244)'
-        const data = [1,Temper(),1]
+        const data = [3,Temper(),2]
+         const contentInset = { top: 100, bottom: 100 }
+
 
         return (
-            <BarChart style={{ height: 200 }} data={data} svg={{ fill }} contentInset={{ top: 50, bottom: 30 }}>
-                <Grid />
+          <View>
+            <Text>SENSORES</Text>
+            
+            <BarChart style={{ height: 400 }} data={data} svg={{ fill }} contentInset={{ top: 10, bottom: 10 }} spacing={0.2} gridMin={0}>
+                <Grid direction={Grid.Direction.HORIZONTAL} />
             </BarChart>
+            <XAxis
+                    style={{ marginHorizontal: -10 }}
+                    data={data}
+                    formatLabel={(value, index) => index}
+                    contentInset={{ left: 100, right: 100 }}
+                    svg={{ fontSize: 30, fill: 'black' }}
+                />
+                        <Text>Temperatura: {Temper()}</Text>
+                </View>
         )
     }
 }
+
 
  
 function HomeScreen() {
@@ -68,6 +97,7 @@ function Sensores(){
 const a = new BarChartExample();
   return (
     a.render()
+
     
    );
 }
@@ -88,6 +118,9 @@ function Actuadores() {
   return (
     
     <View style={styles.container}>
+              <Text>{servo} grados </Text>
+       
+
          <Slider
            style={{width: 200, height: 40}}
           minimumValue={0}
@@ -100,8 +133,35 @@ function Actuadores() {
           value={servo}
           onValueChange={(servo) => firebase.database().ref("dato01").set(servo)}
         />
-        <Text>{servo} grados </Text>
+        <View> 
+
+        </View>
+         
     </View>
+  );
+}
+
+function MoverServo(){
+  const [isEnabled, setIsEnabled] = useState(false);
+//  const relb = false;
+  const toggleSwitch = () => {setIsEnabled(isEnabled => !isEnabled); firebase.database().ref("estado_rele_manual").set(isEnabled)};
+
+  return(
+    <View style={styles.container}>
+      <Button
+        title="Rele01"
+        onPress={() => { firebase.database().ref("estado_rele_manual").set(1);  }}
+      />
+       <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+
+    </View>
+
   );
 }
 const Tab = createBottomTabNavigator();
@@ -144,6 +204,7 @@ const Tab = createBottomTabNavigator();
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Sensores" component={Sensores} />
         <Tab.Screen name="Actuadores" component={Actuadores} />
+        <Tab.Screen name="MoverServo" component={MoverServo} />
 
       </Tab.Navigator>
     </NavigationContainer>
