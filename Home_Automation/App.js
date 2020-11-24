3535f8d29aa755a36218c05089383b3423ecf5b5
surrenderchaos <sandyclaus2015@gmail.com>
2020-11-24 01:59:49 -0500

    codigo con pantallas, sensores y actuadores, sin grafico

--- Home_Automation/App.js
+++ Home_Automation/App.js
@@ -1,16 +1,83 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { AppRegistry, StyleSheet, View, Text } from "react-native";
+import { AppRegistry, StyleSheet, View, Text, processColor  } from "react-native";
 import Slider from '@react-native-community/slider';
-import firebase from './database/firebase'
+import firebase from './database/firebase';
+
+ 
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+ 
+
+function Sensores(){
+  const [temperatura, setTemperatura] = useState(0);
+  // Listen to changes on the firebase database, specifically the "distance" entry
+  useEffect(() => {
+    const getValue = firebase.database().ref("temperatura01");
+    //al parecer existe diferencia en los datos del firebase por lo que se debe cambiar de datos si da un error con este codigo
+    getValue.on("value", snapshot => {
+      // Whenever the value changes on the server, it is also reset on the running app through the variable
+      let value = snapshot.val();
+      setTemperatura(value);
+    });
+        
+
+  }, []);
+
+  return (
+   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>sensorl: {temperatura}</Text>
+    </View> 
+   );
+}
 
 
-export default function App() {
+function Actuadores() {
+  const [servo, setServo] = useState(0);
+  useEffect(() => {
+    const getValue = firebase.database().ref("dato01");
+    getValue.on("value", snapshot => {
+      let value = snapshot.val();
+      setServo(value);
+    });
+        
+
+
+  }, []);
+  return (
+    
+    <View style={styles.container}>
+         <Slider
+           style={{width: 200, height: 40}}
+          minimumValue={0}
+          maximumValue={180}
+          step={10}
+          minimumTrackTintColor="#FFFF00"
+          maximumTrackTintColor="#00FF00"
+         //   value={this.state.value}
+         //    onValueChange= {() => firebase.database().ref("servo01").set(value)}
+          value={servo}
+          onValueChange={(servo) => firebase.database().ref("dato01").set(servo)}
+        />
+        <Text>{servo} grados </Text>
+    </View>
+  );
+}
+const Tab = createBottomTabNavigator();
+
+ export default function App() {
   // Define the state of the component
-const [servo, setServo] = useState(0);
+/*const [servo, setServo] = useState(0);
   useEffect(() => {
     const getValue = firebase.database().ref("dato01");
     getValue.on("value", snapshot => {
@@ -35,29 +102,23 @@ const [servo, setServo] = useState(0);
         
 
 
-  }, []);
+  }, []);*/
 
   
 
 
   return (
+     <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Sensores" component={Sensores} />
+        <Tab.Screen name="Actuadores" component={Actuadores} />
 
-      <View style={styles.container}>
-         <Slider
-    style={{width: 200, height: 40}}
-    minimumValue={0}
-    maximumValue={180}
-    step={10}
-    minimumTrackTintColor="#FFFF00"
-    maximumTrackTintColor="#00FF00"
- //   value={this.state.value}
-//    onValueChange= {() => firebase.database().ref("servo01").set(value)}
-    value={servo}
-     onValueChange={(servo) => firebase.database().ref("dato01").set(servo)}
-  />
-    <Text>slider :   {servo} </Text>
-  <Text>Temperatura: {temperatura}  </Text>
-      </View>
+      </Tab.Navigator>
+    </NavigationContainer>
+      
+
+      
   );
 };
 
